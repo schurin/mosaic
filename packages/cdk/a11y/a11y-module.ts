84032e28ae38c4493aee750bac4baa44fd3563ae
Oleg Pimenov <pimenov.o@gmail.com>
2020-02-17 23:37:05 +0300

    chore: resolution for build

--- packages/cdk/a11y/a11y-module.ts
+++ packages/cdk/a11y/a11y-module.ts
@@ -1,12 +1,11 @@
 import { PlatformModule } from '@angular/cdk/platform';
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { CdkMonitorFocus, FOCUS_MONITOR_PROVIDER } from './focus-monitor/focus-monitor';
 
 
 @NgModule({
-  imports: [CommonModule, PlatformModule],
+  imports: [PlatformModule],
   declarations: [CdkMonitorFocus],
   exports: [CdkMonitorFocus],
   providers: [
